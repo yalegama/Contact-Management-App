863f58694a449b2aa204d625968d5948cd0e1646
Yalegama <nadunyalegama@gmail.com>
2021-08-22 19:30:25 +0530

    4th commit

--- src/sample.js
+++ src/sample.js
@@ -1,19 +1,33 @@
+import React , {useState} from 'react';
 import './sample.css';
 
 
 function Sample() {
 
+    const [Name, setName] = useState('')
+    const [Password, setPassword] = useState('')
 
   return (
       <div>
           <h2>Input your name and password</h2>
-          <input placeholder="Name" type="text"/>
-          <input placeholder="Password" type="password"/>
+          <input onChange={changeName} value={Name} placeholder="Name" type="text"/>
+          <input onChange={changePassword} value={Password} placeholder="Password" type="password"/>
           <br/>
-          <button>Submit</button>
+          <button onClick={Click}>Submit</button>
       </div>
     
   );
+
+  function changeName(event){
+    setName(event.target.value);
+  }
+  function changePassword(event){
+      setPassword(event.target.value);
+  }
+  function Click(){
+      console.log(Name,Password);
+  }
+
 }
 
 export default Sample;
