ade2f445ab04dde67c71caf34ed17d537a3dbacf
Yalegama <nadunyalegama@gmail.com>
2021-08-22 19:22:58 +0530

    third commit

---
+++ src/sample.js
@@ -0,0 +1,19 @@
+import './sample.css';
+
+
+function Sample() {
+
+
+  return (
+      <div>
+          <h2>Input your name and password</h2>
+          <input placeholder="Name" type="text"/>
+          <input placeholder="Password" type="password"/>
+          <br/>
+          <button>Submit</button>
+      </div>
+    
+  );
+}
+
+export default Sample;
